790cc6bd495ad6a8b1c7a64692d768fba47a846a
Greg Spurgeon <spurgeon.greg11@gmail.com>
2021-01-20 22:43:40 -0500

    Added the ablility to add a new plant type to the plant library. The library then only renders the base 24 plants and any plant types the logged in user has created. Added limitation in the add plant type form that the author can only be the logged in user.Added some font coloring that was missing from when we created the new repo.

---
+++ indoorplants/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from indoorplants.models import PlantType
+
+       
+class AddPlantTypeForm(forms.ModelForm):
+
+    class Meta:
+        model = PlantType
+        fields = '__all__'
+        exclude = ['author']
+        widgets = {
+        'name' : forms.TextInput(attrs = {'placeholder': 'Scientific Name'}), 
+        }
--- planters/urls.py
+++ planters/urls.py
@@ -7,7 +7,7 @@ from authentication.views import LoginView, LogoutView, SignUpView, about
 from plantcalendar.views import CalendarView, CreateCalEntry, remove_calendar_entry
 from journal.views import CreateEntryView, remove_entry
 from myuser.views import index_view, profile, edit_profile, delete_profile
-from indoorplants.views import PlantView, PlantTypeView, LibraryView, add_plant, remove_plant, alt_watering
+from indoorplants.views import PlantView, AddPlantType, PlantTypeView, LibraryView, add_plant, remove_plant, alt_watering
 
 
 urlpatterns = [
@@ -32,6 +32,7 @@ urlpatterns = [
     path('remove_entry/<int:entry_id>/', remove_entry, name='remove_entry'),
     path('remove_plant/<int:plant_id>/', remove_plant, name='remove_plant'),
     path('sign_up/', SignUpView.as_view(), name='sign_up'),
+    path('add_plant_type/', AddPlantType.as_view(), name="add_plant_type")
 
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
